5adc043e864e85040fe5bada6532310563132c81
rq <rq@sit.edu.cn>
2023-03-14 17:07:46 +0800

    commit 1

---
+++ src/Git1.java
@@ -0,0 +1,25 @@
+/**
+ * @classname: Git1
+ * @author: rq
+ * @date: 2023/3/13
+ */
+class Git1 {
+   private int g;
+   private int it1;
+
+    public int getG() {
+        return g;
+    }
+
+    void setG(int g) {
+        this.g = g;
+    }
+
+    public int getIt1() {
+        return it1;
+    }
+
+    public void setIt1(int it1) {
+        this.it1 = it1;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Git1 git1 = new Git1();
+        git1.setG(2);
+        System.out.println("Hello Git");
+        System.out.println("Hello Git1");
+
+        System.out.println("Hello Git2");
+
+
+        System.out.println("push  Git2");
+    }
+}
