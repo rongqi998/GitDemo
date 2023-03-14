c11dbefa341857621ea174c743388d2c46428002
rq <rq@sit.edu.cn>
2023-03-14 19:36:21 +0800

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         Git1 git1 = new Git1();
-        git1.setG(2);
+        git1.setG(23);
         System.out.println("Hello Git1");
-        System.out.println("Hello  Git2");
-        System.out.println("push  Git3");
+        System.out.println("Hello Git2");
+        System.out.println("push  Git13");
     }
 }
